eec1852fff32a8f4004cb69ad966b0b7623e917a
shubhmans <shubhman.sinha@gmail.com>
2023-06-19 13:08:34 +0530

    Init project

---
+++ src/test/java/com/envision/demo/SpringbootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.envision.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
